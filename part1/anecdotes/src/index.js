bfdeca6f6d068379bec6422716dfd75f9ce171b7
Emmanuel Valenzuela <emmanuel@kukulha.com>
2021-12-18 21:18:31 -0600

    Exercise 1.6 - 1.14 started

---
+++ part1/anecdotes/src/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = ({anecdotes}) => {  
+  const [selected, setSelected] = useState(0)
+
+  const handleNext = () => {
+    setSelected(Math.floor(Math.random(1) * 5))
+  }
+  
+  return (
+    <div>
+      <p>{anecdotes[selected]}</p>
+      <button>Vote</button>
+      <button onClick={handleNext}>Next anecdote</button>
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
